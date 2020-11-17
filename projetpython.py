1b2254147387b7fbc8564e7d404e9dc7d0c5dbd1
nicolasgasecki <72786577+nicolasgasecki@users.noreply.github.com>
2020-11-17 08:42:49 +0100

    Update projetpython.py

--- projetpython.py
+++ projetpython.py
@@ -74,3 +74,12 @@ df=pd.read_csv('EIVP_KM.csv', sep=';', index_col='sent_at', parse_dates=True)
 A=df.loc['2019-08-11 21:31:40+02:00':'2019-08-14 05:31:35+02:00',"temp"]
 A.plot()
 plt.show()
+
+
+#HUMIDEX (il 'sagit simplement de faire des opérations entre les colonnes du tableau pour calculer l'humidex par sa formule)
+import numpy as np
+df['alpha']=(17.27*df['temp']/(237.7+df['temp'])+np.log(df['humidity']))
+
+df['temp_rosée']=((237.7*df['alpha'])/(17.27-df['alpha']))
+
+df['humidex']= df['temp']+0.555*(6.11*np.exp(5417.7530*((1/273.16)-1/(273.15+df['temp_rosée'])))-10)
