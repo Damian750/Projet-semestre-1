560bbd53190e840b0b4c9beec7ba5211a7989645
Damian Blaszczuk <72787567+Damian750@users.noreply.github.com>
2020-11-29 17:03:28 +0100

    Evolution

--- projetpython.py
+++ projetpython.py
@@ -47,3 +47,28 @@ def humidex():
     plt.ylabel('°C')
     plt.title('Indice humidex au cours du temps')
     plt.show()
+
+    
+##L’évolution d’une variable en fonction du temps
+
+
+#On crée une bibliothèque 'unites' pour pouvoir assimiler à une variable son unité de mesure puis l'afficher sur l'axe des ordonnées. Une bibliotèque 'variable_dico' pour traduire les noms des variables en français pour l'intitulé du graphique:
+
+unites={ 'temp':'°C', 'noise':'dBA' , 'lum':'lux', 'co2':'ppm' , 'humidity':'%' }
+variable_dico={'temp':'température','noise':'bruit','lum':'luminosité','co2':'CO_2','humidity':'humidité relative'}
+
+#La fonction 'evolution()' permet d'afficher l'evolution d'une variable en fonction du temps si on précise la variable, la plage horaire ('d' pour début et 'f' pour fin) et la liste 'L' des capteurs qu'on souhaite analyser:
+
+def evolution():
+        variable=input('entrer une variable (temp,humidity,co2,noise,lum):')
+        d=input('entrer date début (exemple 2020-09):')
+        f=input('entrer date fin:')
+        L=[int(el) for el in input('choix des capteurs de 1 à 6 (ex: 1 3 5):').split()]
+        
+        
+        for k in range(len(L)):  #on parcourt la liste des capteurs sélectionnés
+                df[df['id']==L[k]][variable][d:f].plot(label=f' capteur {L[k]}') # et on dessine le graphique correspondant au capteur 'id' pour la variable et la plage horaire choisies
+        plt.title( 'Evolution de la ' + variable_dico[f'{variable}'])
+        plt.ylabel(unites[f'{variable}'])
+        plt.legend()               
+        plt.show()
