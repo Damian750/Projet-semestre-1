8af3b4b49e7db32402b018719857301a99c1f678
Damian Blaszczuk <72787567+Damian750@users.noreply.github.com>
2020-12-01 01:06:08 +0100

    Ajout indice corrélation

--- projetpython.py
+++ projetpython.py
@@ -134,7 +134,25 @@ def moye():
     A.plot
     plt.show()
     
+def correlation():
     
+    variable1=input('entrer une variable (temp,humidity,co2,noise,lum):')
+    variable2=input('entrer une deuxieme variable (temp,humidity,co2,noise,lum):')
+    
+    f = plt.figure()
+    ax = f.add_subplot(111)
+
+    df[variable1].plot(label='evolution de la ' +variable_dico[f'{variable1}'] )
+    df[variable2].plot(label='evolution de la ' +variable_dico[f'{variable2}'] )#Affichage des deux courbes représentant les deux variables en fonction du temps.
+    
+    indice = df[variable1].corr(df[ variable2]) #indice de corrélation entre les deux variables
+
+    
+    plt.text(0.5,0.5,f"l'indice de correlation est {indice}",horizontalalignment='center',
+     verticalalignment='center', transform = ax.transAxes, fontsize=7, color='r')#Indication dans de la valeur de l’indice de corrélation
+
+    plt.legend()
+    plt.show() 
        
     
 def humidex():
